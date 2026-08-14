a992f8001a72c56ec4b02101548919dd42c3ed4f
Amanda Ferreira <ferreiradejesusamanda@gmail.com>
2026-08-14 17:11:17 -0300

    Implementação da função de filtrar voos e criação do menu

--- atividade-03-simulando-SGBD.py
+++ atividade-03-simulando-SGBD.py
@@ -15,6 +15,7 @@ def full_table_scan():
     except FileNotFoundError:
         print("Arquivo não encontrado.")
         
+        
 def buscar_por_id():
     
     id_procurado = input("ID para procura: ")
@@ -39,3 +40,59 @@ def buscar_por_id():
                 
     except FileNotFoundError:
             print("Arquivo não encontrado.")
+            
+
+def filtrar_voos():
+    procurar_voo = input("Destino para procura: ")
+    encontrou = False
+    
+    try:
+        with open(ARQUIVO_VOOS, "r", encoding="utf-8") as arquivo:
+            cabecalho = next(arquivo)
+                    
+            for linha in arquivo:
+                dados = linha.strip().split(";")
+                        
+                dest_procurado = dados[2]
+                        
+                if dest_procurado.strip().lower() == procurar_voo.strip().lower():
+                    if not encontrou:
+                        print(f"\n{'Voo':<6} {'Horario': <8}")
+                        print("-" * 20)
+                        encontrou = True
+                        
+                    num_voo, horario = dados[1], dados[4]
+                    print(f"{num_voo:<6} {horario:<8}")
+                    
+            if not encontrou:
+                print("\n Nenhum voo encontrado para este destino.")
+                        
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        
+
+def menu():
+    while True:
+        print("\n--- Gerenciamento de Voos ---")
+        print("1 - Listar todos os voos")
+        print("2 - Buscar voo por ID")
+        print("3 - Filtrar voos por Destino")
+        print("0 - Sair")
+        
+        opcao = input("\nEscolha uma opção: ")
+        
+        if opcao == "1":
+            full_table_scan()
+        elif opcao == "2":
+            buscar_por_id()
+        elif opcao == "3":
+            filtrar_voos()
+        elif opcao == "0":
+            print("Sistema encerrado.")
+            break
+        
+        else:
+            print("Opção inválida!")
+        
+            
+menu()
